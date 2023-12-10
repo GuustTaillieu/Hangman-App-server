79cf5057f422fe5574d58ef714d471e0c15a8b23
Guust Taillieu <guust@taillieusauw.net>
2023-12-10 16:53:06 +0100

    Edit CORS for controller and websocket

--- src/main/java/be/howest/ti/hangman/controller/HomeController.java
+++ src/main/java/be/howest/ti/hangman/controller/HomeController.java
@@ -1,12 +1,14 @@
 package be.howest.ti.hangman.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(value = "*", allowedHeaders = "*")
 public class HomeController {
 
-    @GetMapping("/")
+    @GetMapping("/status")
     public String home() {
         return "Up and running";
     }
